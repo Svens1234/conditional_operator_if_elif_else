530198c6d3eda72fe53ad56426a86064de0e59cf
Svenden123 <buhrus1@yandex.ru>
2021-08-02 18:23:48 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+x =4
+if x>3:
+    print('x>3')
+    print('I\'m happy')
+elif x<3:
+    print('x<3')
+else:
+    print('x==3')
+
+
+day_time = 'midnight'
+
+if day_time == 'morning':
+    print('Monster wakes up')
+if day_time == 'afternoon':
+    print('Monster is walking')
+if day_time == 'evening':
+    print('Monster is eating')
+if day_time == 'night':
+    print('Monster is sleeping')
+else:
+    print('Monster is doing something')
+
+x=40
+if x%2==0:
+    print('x is even')
+else:
+    print('x is odd')
+print('Some text')
+
+
+x=41
+if x%2==0:
+    print('x is even')
+else:
+    print('x is odd')
+print('Some text')
+
+
+user_input = input('Input something ')
+if user_input == 'Hello':
+    print('Hello!Nice to meet you')
+
+lucky_number = input('Enter a number ')
+if lucky_number:
+    print(lucky_number + 'is your lucky number!')
+else:
+    print('You have to enter some number, please try again')
+
